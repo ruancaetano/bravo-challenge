12973737c4e7ad5c1602e98bb98a0a9503ede659
Ruan Caetano <ruansouza_caetano@hotmail.com>
2022-10-14 01:45:23 -0300

    feat: add currency usecase

---
+++ internal/domain/usecases/add_currency/add_currency.go
@@ -0,0 +1,49 @@
+package add_currency
+
+import (
+	"github.com/ruancaetano/challenge-bravo/internal/domain/entities"
+	"github.com/ruancaetano/challenge-bravo/internal/domain/repositories"
+)
+
+type AddCurrencyUseCaseInputDTO struct {
+	Code                  string
+	Type                  entities.CurrencyType
+	DollarBasedProportion float64
+}
+
+type AddCurrencyUseCaseOutputDTO struct {
+	ID                    string
+	Code                  string
+	Type                  entities.CurrencyType
+	DollarBasedProportion float64
+}
+
+type AddCurrencyUseCase struct {
+	CurrencyRepository repositories.CurrencyRepository
+}
+
+func NewAddCurrencyUseCase(currencyRepository repositories.CurrencyRepository) *AddCurrencyUseCase {
+	return &AddCurrencyUseCase{
+		CurrencyRepository: currencyRepository,
+	}
+}
+
+func (u *AddCurrencyUseCase) Execute(input *AddCurrencyUseCaseInputDTO) (*AddCurrencyUseCaseOutputDTO, error) {
+	currency, err := entities.NewCurrency(input.Code, input.Type, input.DollarBasedProportion)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.CurrencyRepository.Create(currency)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &AddCurrencyUseCaseOutputDTO{
+		ID:                    currency.ID,
+		Code:                  currency.Code,
+		Type:                  currency.Type,
+		DollarBasedProportion: currency.DollarBasedProportion,
+	}, nil
+}
---
+++ internal/domain/usecases/add_currency/add_currency_test.go
@@ -0,0 +1,49 @@
+package add_currency_test
+
+import (
+	"testing"
+
+	"github.com/ruancaetano/challenge-bravo/internal/domain/entities"
+	"github.com/ruancaetano/challenge-bravo/internal/domain/usecases/add_currency"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type CurrencyRepositoryMock struct {
+	mock.Mock
+}
+
+func (r *CurrencyRepositoryMock) Get(code string) (*entities.Currency, error) {
+	args := r.Mock.Called(code)
+	return args.Get(0).(*entities.Currency), args.Error(1)
+}
+
+func (r *CurrencyRepositoryMock) Create(currency *entities.Currency) error {
+	args := r.Mock.Called(currency)
+	return args.Error(0)
+}
+
+func (r *CurrencyRepositoryMock) Delete(code string) error {
+	args := r.Mock.Called(code)
+	return args.Error(0)
+}
+
+func TestAddCurrencyUseCase_Execute(t *testing.T) {
+	currencyRepositoryMock := &CurrencyRepositoryMock{}
+
+	currencyRepositoryMock.On("Create", mock.Anything).Return(nil)
+
+	usecase := add_currency.NewAddCurrencyUseCase(currencyRepositoryMock)
+
+	output, err := usecase.Execute(&add_currency.AddCurrencyUseCaseInputDTO{
+		Code:                  "USD",
+		Type:                  entities.FIAT,
+		DollarBasedProportion: 0,
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, output.ID)
+
+	currencyRepositoryMock.AssertExpectations(t)
+	currencyRepositoryMock.AssertNumberOfCalls(t, "Create", 1)
+}
