35e6c9a3249eea55f49282cac84f82dc87bb615d
Ruan Caetano <ruansouza_caetano@hotmail.com>
2022-10-15 01:34:44 -0300

    test: add get-currency-usecase integration tests

---
+++ domain/usecases/get_currency/get_currency_integration_test.go
@@ -0,0 +1,62 @@
+package get_currency_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/ruancaetano/challenge-bravo/domain/entities"
+	"github.com/ruancaetano/challenge-bravo/domain/usecases/get_currency"
+	"github.com/ruancaetano/challenge-bravo/infra/database/sqlite"
+	"github.com/ruancaetano/challenge-bravo/infra/database/sqlite/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+var manager = sqlite.NewDBManager()
+
+func setUp() *repositories.SqliteCurrencyRepository {
+	manager.Open(":memory:")
+
+	craeteTable(manager.DB)
+
+	repository := repositories.NewSqliteCurrencyRepository(manager)
+
+	return repository
+}
+
+func craeteTable(db *sql.DB) {
+	queryString := `CREATE TABLE currencies (
+		"id" string,
+		"code" string unique,
+		"type" string,
+		"dollar_based_proportion" float
+	);`
+
+	stmt, err := db.Prepare(queryString)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func TestGetCurrencyUseCaseIntegration_Execute(t *testing.T) {
+	repository := setUp()
+	defer manager.Close()
+
+	usecase := get_currency.NewGetCurrencyUseCase(repository)
+
+	currency, err := entities.NewCurrency("USD", entities.FIAT, 0)
+	assert.Nil(t, err)
+
+	err = repository.Create(currency)
+	assert.Nil(t, err)
+
+	output, err := usecase.Execute(&get_currency.GetCurrencyUseCaseInputDTO{
+		Code: "USD",
+	})
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, output.Code, "USD")
+}
